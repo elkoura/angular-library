8dec5cac4b500c220d62d6455fcac510a4b9a85c
Ayoub Benziza <64789925+AyoubBenziza@users.noreply.github.com>
2024-08-01 21:52:23 +0200

    add: Navbar Util & NavLinks Generic

---
+++ src/app/utils/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input} from '@angular/core';
+import {NgClass} from "@angular/common";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  @Input() links: { icon: string, label: string, url: string }[] = [];
+  isNavVisible: boolean = true;
+
+  toggleNav() {
+    this.isNavVisible = !this.isNavVisible;
+  }
+}

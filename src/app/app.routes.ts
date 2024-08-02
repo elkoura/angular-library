6cd05174b152200fb626b527d9fb0380fb6409d7
Ayoub Benziza <64789925+AyoubBenziza@users.noreply.github.com>
2024-08-02 17:00:26 +0200

    add: Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import {BookFormComponent} from "@components/book-form/book-form.component";
 import { BookListComponent } from '@components/book-list/book-list.component';
+import {HomeComponent} from "@components/home/home.component";
+import {BookDetailComponent} from "@components/book-detail/book-detail.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: '', component: HomeComponent, title: 'Home' },
   { path: 'books', component: BookListComponent },
   { path: 'books/add', component: BookFormComponent, title: 'Add Book' },
-  { path: 'books/:id/edit', component: BookFormComponent, title: 'Edit Book' }
+  { path: 'books/:id/edit', component: BookFormComponent, title: 'Edit Book' },
+  { path: 'books/:id/view', component: BookDetailComponent, title: 'Book Details' }
 ];

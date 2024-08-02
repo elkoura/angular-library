c234d10f0cbfc269147558169622556ca5c77c83
Ayoub Benziza <64789925+AyoubBenziza@users.noreply.github.com>
2024-08-02 17:27:00 +0200

    fix: BookDetails Component

--- src/app/components/book-detail/book-detail.component.ts
+++ src/app/components/book-detail/book-detail.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Book } from '@models/book.model';
 import { BookService } from '@services/book.service';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-book-detail',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.scss']
 })
